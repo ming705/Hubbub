0a1504a1e09e28f0d9c619ac39d2513ffdb6cbcc
Mingsheng Xie <mingsheng.xie@gmail.com>
2015-12-02 18:22:50 -0500

    Install dynamic scaffolding templates, seed DB with test data and Query the DB

--- grails-app/controllers/com/grailsinaction/UserController.groovy
+++ grails-app/controllers/com/grailsinaction/UserController.groovy
@@ -4,4 +4,33 @@ class UserController {
 
     static scaffold = User
 
+    def search() {}
+
+    def results(String loginId) {
+        def users = User.where { loginId =~ "%${loginId}%" }.list()
+        return [ users: users,
+                 term: params.loginId,
+                 totalUsers: User.count() ]
+    }
+    
+    def advSearch() {}
+
+    def advResults() {
+        def profileProps = Profile.metaClass.properties*.name
+        def profiles = Profile.withCriteria {
+            "${params.queryType}" {
+
+                params.each { field, value ->
+
+                    if (profileProps.grep(field) && value) {
+                        ilike(field, value)
+                    }
+                }
+
+            }
+
+        }
+        [ profiles : profiles ]
+
+    }
 }
