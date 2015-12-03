fc8d7f6727f7cabf1410e4c09bd2db3a7e072112
Mingsheng Xie <mingsheng.xie@gmail.com>
2015-12-03 14:12:36 -0500

    Add timeline() and addPost() actions to PostController, also timeline view, redirect() and flash scope

--- grails-app/controllers/com/grailsinaction/PostController.groovy
+++ grails-app/controllers/com/grailsinaction/PostController.groovy
@@ -3,5 +3,38 @@ package com.grailsinaction
 class PostController {
 
     static scaffold = Post
+    
+    static defaultAction = "home"
 
+    def home() {
+        if (!params.id) {
+            params.id = "chuck_norris"
+        }
+        redirect(action: 'timeline', params: params)
+    }
+    
+    def timeline() {
+        def user = User.findByLoginId(params.id)
+        if (!user) {
+            response.sendError(404)
+        } else {
+            [ user : user ]
+        }
+    }
+
+    def addPost() {
+        def user = User.findByLoginId(params.id)
+        if (user) {
+            def post = new Post(params)
+            user.addToPosts(post)
+            if (user.save()) {
+                flash.message = "Successfully created Post"
+            } else {
+                flash.message = "Invalid or empty post"
+            }
+        } else {
+            flash.message = "Invalid User Id"
+        }
+        redirect(action: 'timeline', id: params.id)
+    }
 }
